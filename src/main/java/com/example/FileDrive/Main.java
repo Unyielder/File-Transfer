3ae7c830a4c54e5201e53f6ff2955935f5cdf582
Mohamed Temacini <mo.temacini@gmail.com>
2022-01-11 23:19:35 -0500

    initial setup

--- src/main/java/com/example/FileDrive/Main.java
+++ src/main/java/com/example/FileDrive/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileDriveApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FileDriveApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/FileDrive/bucket/Bucket.java
@@ -0,0 +1,15 @@
+package com.example.FileDrive.bucket;
+
+public enum Bucket {
+
+    PROFILE_IMAGE("file-drive-001");
+    private final String bucketName;
+
+    Bucket(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return this.bucketName;
+    }
+}

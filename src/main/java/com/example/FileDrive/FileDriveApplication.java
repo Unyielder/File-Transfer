c269c707870742826be1c66949c272006f0428aa
Mohamed Temacini <mo.temacini@gmail.com>
2022-01-11 23:03:40 -0500

    first commit

---
+++ src/main/java/com/example/FileDrive/FileDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.FileDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FileDrive/FileDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FileDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

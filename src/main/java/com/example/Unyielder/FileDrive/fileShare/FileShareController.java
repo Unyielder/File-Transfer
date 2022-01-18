2a0d0d9cda73557ebe80db3e3a3093479a8eabb5
Mohamed Temacini <mo.temacini@gmail.com>
2022-01-17 23:18:55 -0500

    Adding functionality to zip multiple incoming files from dropzone

--- src/main/java/com/example/Unyielder/FileDrive/fileShare/FileShareController.java
+++ src/main/java/com/example/Unyielder/FileDrive/fileShare/FileShareController.java
@@ -2,8 +2,13 @@ package com.example.Unyielder.FileDrive.fileShare;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/file-sharing")
@@ -22,8 +27,7 @@ public class FileShareController {
             consumes= MediaType.MULTIPART_FORM_DATA_VALUE,
             produces= MediaType.APPLICATION_JSON_VALUE
     )
-    public void fileUpload(@RequestParam("file") MultipartFile file) {
-        fileShareService.upload(file);
+    public void fileUpload(@RequestParam("file") List<MultipartFile> fileArray) throws IOException {
+        fileShareService.upload(fileArray);
     }
-
 }
--- src/main/java/com/example/Unyielder/FileDrive/fileShare/FileShareService.java
+++ src/main/java/com/example/Unyielder/FileDrive/fileShare/FileShareService.java
@@ -1,17 +1,23 @@
 package com.example.Unyielder.FileDrive.fileShare;
 
+import com.amazonaws.services.iotevents.model.Input;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import com.example.Unyielder.FileDrive.bucket.Bucket;
+import org.springframework.web.multipart.MultipartRequest;
 
 @Service
 public class FileShareService {
@@ -23,41 +29,69 @@ public class FileShareService {
         this.s3 = s3;
     }
 
-    public void upload(MultipartFile file) {
-        if(file.isEmpty()) {
-            throw new IllegalStateException("No file has been uploaded");
-        }
-        Optional<Map<String, String>> optionalMetadata = extractMetadata(file);
-        ObjectMetadata metadata = new ObjectMetadata();
-
-        optionalMetadata.ifPresent(map -> {
-            if(!map.isEmpty()) {
-                map.forEach(metadata::addUserMetadata);
-            }
-        });
-
+    public void upload(List<MultipartFile> fileArray) throws IOException {
+        fileArray.forEach(file -> System.out.println(file.getOriginalFilename()));
         String path = String.format(
-                "%s/file-sharing/%s",
-                Bucket.PROFILE_IMAGE.getBucketName(), file.getOriginalFilename()
+                "%s/file-sharing",
+                Bucket.PROFILE_IMAGE.getBucketName()
         );
 
-        try {
-            s3.putObject(
-                    path,
-                    file.getOriginalFilename(),
-                    file.getInputStream(),
-                    metadata
-            );
-        } catch(IOException e) {
-            throw new IllegalStateException("Unable to store file to s3");
+        if(fileArray.size() == 0) {
+            throw new IllegalStateException("No file has been uploaded");
+
+        } else if(fileArray.size() == 1) {
+            MultipartFile file = fileArray.get(0);
+            ObjectMetadata metadata = getMetadata(file);
+            storeFile(path, file.getOriginalFilename(), file.getInputStream(), metadata);
+
+        } else {
+            File zipFile = new File("zipFile.zip");
+            ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)));
+            for(MultipartFile file : fileArray) {
+                InputStream inputStream = file.getInputStream();
+
+                ZipEntry zipEntry = new ZipEntry(Objects.requireNonNull(file.getOriginalFilename()));
+                zos.putNextEntry(zipEntry);
+
+                byte[] bytes = new byte[1024];
+                int length;
+                while((length = inputStream.read(bytes)) > -1) {
+                    zos.write(bytes, 0, length);
+                }
+
+            }
+            BufferedInputStream zipFileInputStream = new BufferedInputStream(new FileInputStream(zipFile));
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentDisposition("attachment; filename=zipFile.zip");
+            storeFile(path, "zipFile.zip", zipFileInputStream, metadata);
+            zos.close();
         }
 
     }
 
-    private Optional<Map<String, String>> extractMetadata(MultipartFile file) {
+    private void storeFile(String path, String fileName, InputStream inputStream, ObjectMetadata metadata) {
+        s3.putObject(
+                path,
+                fileName,
+                inputStream,
+                metadata
+        );
+    }
+
+    private ObjectMetadata getMetadata(MultipartFile file) {
         Map<String, String> metadata = new HashMap<>();
         metadata.put("Content-Type", file.getContentType());
         metadata.put("Content-Length", String.valueOf(file.getSize()));
-        return Optional.of(metadata);
+
+        Optional<Map<String, String>> optionalMetadata = Optional.of(metadata);
+        ObjectMetadata objMetadata = new ObjectMetadata();
+
+        optionalMetadata.ifPresent(map -> {
+            if(!map.isEmpty()) {
+                map.forEach(objMetadata::addUserMetadata);
+            }
+        });
+        return objMetadata;
     }
 }

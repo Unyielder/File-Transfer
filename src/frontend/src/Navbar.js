76f06ef0a3b0bf3842b6190d85e4cd9dbb2d8dfa
Mohamed Temacini <mo.temacini@gmail.com>
2022-03-30 23:10:56 -0400

    Back to upload navigation

--- src/frontend/src/Navbar.js
+++ src/frontend/src/Navbar.js
@@ -1,7 +1,9 @@
+import { BASE_URL_FRONTEND } from './config/env.js';
+
 export default function Navbar() {
     return (
         <div className="nav">
-            <h3>File Transfer</h3>
+            <a href={`${BASE_URL_FRONTEND}/#/upload`}><h3>File Transfer</h3></a>
         </div>
     )
 }
